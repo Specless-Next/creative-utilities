2646982242b8590dfa572b1ba29ea740aca1e5c5
Shashank <shashank@gospecless.com>
2020-12-09 22:19:05 -0600

    add docs

--- src/native-css.ts
+++ src/native-css.ts
@@ -3,15 +3,16 @@ const NATIVE_CSS_URL =
   'https://us-central1-specless-next.cloudfunctions.net/ad-server-native-css';
 
 /**
- * Generate a link to Native CSS Ad Server Function
+ * Generate a link to Native CSS Ad Server Function.
+ *
  * @param orgId - Organization Id
- * @param domain - Domain Url - **DO NOT ENTER http or https**
+ * @param domain - Domain Url - **Do not prefix with https or http**
  * @param cacheTimeout - Enter timeout for caching in seconds - Default to 6 hours
  */
 const formNativeCssUrl = (
   orgId: string,
   domain: string,
-  cacheTimeout = 60 * 60
+  cacheTimeout = 6 * 60 * 60
 ) => {
   const cacheValue = Math.floor(Date.now() / (1000 * cacheTimeout));
   const nativeUrl = `${NATIVE_CSS_URL}?org=${orgId}&domain=${domain}&cache=${cacheValue}`;
