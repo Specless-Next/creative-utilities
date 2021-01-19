c79780df4af0b6ac5e3d405f2ab0ab62164b6ed0
Shashank Sharma <shashank@augment.dev>
2021-01-19 10:21:08 -0600

    expose cookie

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import {checkCookie, setCookie} from './cookie';
 import {formNativeCssUrl} from './native-css';
 import {setupDfpSlot} from './google';
 import {formPanelUrl, createIFrame} from './panel';
@@ -7,9 +8,11 @@ import {createStyleTag} from './style';
  * Export all the utilities function
  */
 export {
+  checkCookie,
   createIFrame,
   createStyleTag,
   formNativeCssUrl,
   formPanelUrl,
+  setCookie,
   setupDfpSlot,
 };
